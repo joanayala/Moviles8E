240004109763acb00938b77b549da84c45d72f68
joanayala <jflaco85@gmail.com>
2020-03-26 21:49:54 -0500

    Create Splash Screen Activity Class

--- ColorsApp/app/src/main/java/com/example/colors/MainActivity.java
+++ ColorsApp/app/src/main/java/com/example/colors/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.colors;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final int SPLASH_TIME = 3000; //3 Seconds.
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, PaletteActivity.class);
+                finish();
+            };
+        }, SPLASH_TIME);
     }
 }
