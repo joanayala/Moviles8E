0041bbdf977c1fe0fbba58559eda815e22ee9501
joanayala <jflaco85@gmail.com>
2020-03-26 20:57:08 -0500

    Create OptionMenu Colors

--- ColorsApp/app/src/main/java/com/example/colors/PaletteActivity.java
+++ ColorsApp/app/src/main/java/com/example/colors/PaletteActivity.java
@@ -3,6 +3,8 @@ package com.example.colors;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class PaletteActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class PaletteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_palette);
+    }
+
+    //This method lets us show the menu on mobile app
+    //Note: Only in this activity
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu1, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
