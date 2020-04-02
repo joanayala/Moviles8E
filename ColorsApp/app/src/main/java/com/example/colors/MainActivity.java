a0c8b801898e39ec98ae253495cf85d9e5f1743c
joanayala <jflaco85@gmail.com>
2020-04-01 21:30:10 -0500

    Coding seekbars actions

--- ColorsApp/app/src/main/java/com/example/colors/MainActivity.java
+++ ColorsApp/app/src/main/java/com/example/colors/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 Intent intent = new Intent(MainActivity.this, PaletteActivity.class);
+                startActivity(intent);
                 finish();
             };
         }, SPLASH_TIME);
--- ColorsApp/app/src/main/java/com/example/colors/PaletteActivity.java
+++ ColorsApp/app/src/main/java/com/example/colors/PaletteActivity.java
@@ -2,24 +2,80 @@ package com.example.colors;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.SeekBar;
 
-public class PaletteActivity extends AppCompatActivity {
+public class PaletteActivity extends AppCompatActivity
+        implements SeekBar.OnSeekBarChangeListener {
+
+    //Variables
+    private SeekBar sbrRed = null;
+    private SeekBar sbrGreen = null;
+    private SeekBar sbrBlue = null;
+    private SeekBar sbrAlpha = null;
+    private View vieColors = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_palette);
+
+        //Get Components ID's
+        sbrRed = findViewById(R.id.sbrRed);
+        sbrGreen = findViewById(R.id.sbrGreen);
+        sbrBlue = findViewById(R.id.sbrBlue);
+        sbrAlpha = findViewById(R.id.sbrAlpha);
+        vieColors = findViewById(R.id.vieColors);
+
+        sbrRed.setOnSeekBarChangeListener(this);
+        sbrGreen.setOnSeekBarChangeListener(this);
+        sbrBlue.setOnSeekBarChangeListener(this);
+        sbrAlpha.setOnSeekBarChangeListener(this);
+
     }
+
+    //#############################################
+    //MENUS
+    //#############################################
 
     //This method lets us show the menu on mobile app
     //Note: Only in this activity
+    //Note: This method has not actions.
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu1, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+
+    //#############################################
+    //SEEKBAR'S
+    //#############################################
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean bo) {
+        int r = sbrRed.getProgress();
+        int g = sbrGreen.getProgress();
+        int b = sbrBlue.getProgress();
+        int a = sbrAlpha.getProgress();
+
+        int color = Color.argb(a,r,g,b);
+        vieColors.setBackgroundColor(color);
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
